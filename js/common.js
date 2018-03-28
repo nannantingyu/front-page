bd3a935e2f7d2b63e01da1d9d298199e21747581
CAPTAIN_TU\x <nannantingyu@gmail.com>
2018-03-28 17:41:47 +0800

    臭臭的第一个外快

---
+++ js/common.js
@@ -0,0 +1,33 @@
+var baseFontSize = 100;
+var baseWidth = 320;
+var clientWidth = document.documentElement.clientWidth || window.innerWidth;
+var innerWidth = Math.max(Math.min(clientWidth, 480), 320);
+var rem = 100;
+if (innerWidth > 362 && innerWidth <= 375) {
+    rem = Math.floor(innerWidth / baseWidth * baseFontSize * 0.9);
+}
+if (innerWidth > 375) {
+    rem = Math.floor(innerWidth / baseWidth * baseFontSize * 0.84);
+}
+window.__baseREM = rem;
+document.querySelector('html').style.fontSize = rem + 'px';
+
+$(function(){
+   setTimeout(function() {
+       $(".avg").animate({width:'90%'}, "slow");
+       $(".mine").animate({width:'50%'}, "slow");
+   }, 1000);
+
+   $(".question").click(function() {
+       if($(".hide-info").is(":visible")) {
+           $(".hide-info").slideUp();
+       }
+       else {
+           $(".hide-info").slideDown();
+       }
+   });
+
+   $(".hide-info").click(function(){
+       $(".hide-info").slideUp();
+   });
+});
